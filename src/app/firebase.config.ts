df47f188e3573e1df35ab6f4612d6477509ea4b9
rafszul <rafszul@gmail.com>
2017-02-14 17:55:09 +0000

    firebase

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,8 @@
 // Must export the config
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: 'AIzaSyB1I-0vWuAGzwYfuBvu9lrg7iPQP0jjuzI',
+  authDomain: 'yuarte-4cb0d.firebaseapp.com',
+  databaseURL: 'https://yuarte-4cb0d.firebaseio.com',
+  storageBucket: 'yuarte-4cb0d.appspot.com',
+  messagingSenderId: '997996667906'
 };
